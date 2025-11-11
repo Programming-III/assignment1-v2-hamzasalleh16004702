9a062245d60269b113b4e2787e6ccb31a18a6b66
hamzasalleh16004702 <hamza.salleh@student.giu-uni.de>
2025-11-11 09:21:30 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,16 +4,36 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
-
-
-
-
-
-
-
-
+class course{
+  private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+  public:
+    course(string cc, string cn, int ms, Student* s, int cs){
+      this -> courseCode = cc;
+      this -> courseName = cn;
+      this -> maxStudent = ms;
+      this -> students = s;
+      this -> currentStudents = cs;
+    }
+    void addStudent(const Student& s){
+      if(currentStudents == maxStudent)
+        cout << "error, students reached max" << endl;
+      else
+        currentStudents += 1;
+    }
+    void displayCourseInfo(){
+      cout << "course code: " << courseCode << endl;
+      cout << "course name: " << courseName << endl;
+      cout << "max students: " << maxStudents << endl;
+      cout << "current students: " << currentStudents << endl;
+    }
+}
+        
 
 
 
