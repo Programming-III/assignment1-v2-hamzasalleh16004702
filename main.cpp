daefd00dd0337134cc3ddff5d8b9a483babff515
hamzasalleh16004702 <hamza.salleh@student.giu-uni.de>
2025-11-11 09:51:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
-
+Person p = new Person(Omar Nabil, 2202);
 
 
 
@@ -18,21 +18,21 @@ using namespace std;
 
 
 // ==================== Student Class Implementation ====================
-
+Student s = new Student(2, informatics);
 
 
 
 
 
 // ==================== Instructor Class Implementation ====================
-
+Instructor x = new Instructor(Computer Science, 5);
 
 
 
 
 
 // ==================== Course Class Implementation ====================
-
+Course c = new Course()
 
 
 
